288f7f11a0176723e426627ad7c49495d4f2e4de
amir.nurhakim <207092@student.upm.edu.my>
2024-07-01 15:30:47 +0800

    add Route Wildcard to listen for Product Id

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 Route::get('/', function () {
     return view('home');
@@ -11,5 +12,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/product', function () {
-    return view('product');
+    return view('product', ['products' => Product::all()]);
+});
+
+Route::get('/product/{id}', function ($id) {
+    return view('product-detail', ['product' => Product::find($id)]);
 });
