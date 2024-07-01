7c52dcb034a782733905c450197ce7f5933ea90a
amir.nurhakim <207092@student.upm.edu.my>
2024-07-01 22:07:46 +0800

    implements Product Eloquent Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,34 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Product
-{
-    public static function all(): array {
-        return [
-            [
-                'id'=>1,
-                'name'=>'Mouse',
-                'price'=>'RM 120.00'
-            ],
-            [
-                'id'=>2,
-                'name'=>'Keyboard',
-                'price'=>'RM 250.00'
-            ],
-            [
-                'id'=>3,
-                'name'=>'Monitor',
-                'price'=>'RM 780.00'
-            ]
-        ];
-    }
-    public static function find(int $id): array {
-        $product = Arr::first(static::all(), fn($product)=>$product['id']==$id);
-        
-        if(! $product) {
-            abort(404);   
-        }
-
-        return $product;
-    }
+class Product extends Model {
+    protected $table = 'products';
+    protected $fillable = ['name','price'];
 }
