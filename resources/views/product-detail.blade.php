45b61b6f6c504124cf15f7f68c57327d32d1fc95
amir.nurhakim <207092@student.upm.edu.my>
2024-07-02 14:36:00 +0800

    remove redundant views

--- resources/views/product-detail.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>Product</x-slot:heading>
-    
-    <h2 class="font-bold text-lg">{{ $product['name']}}</h2>
-    <p>This product has the average price of {{ $product['price']}}</p>
-</x-layout>
