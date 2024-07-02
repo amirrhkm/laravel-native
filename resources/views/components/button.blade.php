e31e4b3a4226626abbbfd5d8269676e2b76b7477
amir.nurhakim <207092@student.upm.edu.my>
2024-07-02 15:38:01 +0800

    add component for button tailwindcss style

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,1 @@
+<a {{ $attributes->merge(['class'=>'text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2']) }}>{{ $slot }}</a>
