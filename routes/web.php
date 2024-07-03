318f35ff3f39b4016a8581ec4c308b4679d518a1
amir.nurhakim <207092@student.upm.edu.my>
2024-07-03 10:18:41 +0800

    simplify & implement ProductController resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/about', 'about');
 
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/product/create', function () {
-    return view('products.create');
-});
-
-Route::post('/product', function () {
-    request()->validate([
-        'name'=> ['required','min:8'],
-        'price'=> ['required'],
-    ]);
-    Product::create([
-        'name'=>request('name'),
-        'price'=>request('price'),
-        'brand_id'=>1
-    ]);
-    return redirect('/product');
-});
-
-Route::get('/product', function () {
-    $products = Product::with('brand')->latest()->paginate(5);
-    return view('products.index', ['products' => $products]);
-});
-
-Route::get('/product/{id}', function ($id) {
-    return view('products.details', ['product' => Product::find($id)]);
-});
-
-Route::get('/product/{id}/edit', function ($id) {
-    return view('products.edit', ['product' => Product::find($id)]);
-});
-
-Route::patch('/product/{id}', function ($id) {
-    request()->validate([
-        'name'=> ['required'],
-        'price'=> ['required'],
-    ]);
-
-    $product = Product::findOrFail($id);
-    $product->name = request('name');
-    $product->price = request('price');
-    $product->save();
-
-    $product->update([
-        'name'=> request('name'),
-        'price'=> request('price'),
-    ]);
-
-    return redirect('/product/'. $product->id);
-});
-
-Route::delete('/product/{id}', function ($id) {
-    Product::findOrFail($id)->delete();
-    return redirect('/product');
-});
+Route::resource('product', ProductController::class);
