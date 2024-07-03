4c26d98d749a4a5965babc0ddab6daae04ee38d3
amir.nurhakim <207092@student.upm.edu.my>
2024-07-03 15:26:10 +0800

    complete store method for login form

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,6 +14,24 @@ class SessionController extends Controller
     }
 
     public function store() {
+        $attr = request()->validate([
+            'email'=>['required','email'],
+            'password'=>['required'],
+        ]);
 
+        if(!Auth::attempt($attr)) {
+            throw ValidationException::withMessages([
+                'email'=>'Invalid email or password. Please try again.',
+                'password'=>'Invalid email or password. Please try again.'
+            ]);
+        }
+
+        request()->session()->regenerate();
+        
+        return redirect('/product');
+    }
+    public function destroy() {
+        Auth::logout();
+        return redirect("/");
     }
 }
