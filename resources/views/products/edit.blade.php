5dac2cf696b70c2a04df397a2a056d1d69b2eb0d
amir.nurhakim <207092@student.upm.edu.my>
2024-07-03 12:05:20 +0800

    utilise form-related components

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -10,45 +10,23 @@
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Product name</label>
-                        <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input 
-                                type="text" 
-                                name="name" 
-                                id="name" 
-                                class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" 
-                                placeholder="Deathadder V3 Pro"
-                                value="{{ $product->name }}" 
-                                required>
-                            </div>
-                            @error('name')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
-                    </div>
-                </div>
 
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Product price</label>
+                    <x-form-field>
+                        <x-form-label for="name">Product name</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input
-                                type="text"
-                                name="price"
-                                id="price"
-                                class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                placeholder="RM 235.85"
-                                value="{{ $product->price }}"
-                                required>
-                            </div>
-                            @error('price')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input  type="text" name="name" id="name" placeholder="Deathadder V3 Pro" value="{{ $product->name }}" required />
+                            <x-form-error name='name'></x-form-error>
                         </div>
-                    </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="name">Product price</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input  type="text" name="price" id="price" placeholder="123.45" value="{{ $product->price }}" required />
+                            <x-form-error name='name'></x-form-error>
+                        </div>
+                    </x-form-field>
+
                 </div>
             </div>
 
@@ -64,10 +42,7 @@
                 <div class="flex items-center gap-x-6">
                     <a href="/product/{{$product->id}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
                     <div>
-                        <button
-                            type="submit"
-                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update
-                        </button>
+                        <x-form-button>Update</x-form-button>
                     </div>
                 </div>
                 
