d4a6afc9472fa5228c69a18c660d758d62a9f749
amir.nurhakim <207092@student.upm.edu.my>
2024-07-03 12:06:09 +0800

    add listener/handler for register & login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 Route::view('/', 'home');
 Route::view('/about', 'about');
 
 Route::resource('product', ProductController::class);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class,'create']);
+Route::post('/login', [SessionController::class,'store']);
