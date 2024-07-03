2174d0efbb97e751c6fb57195b90aa6a9c75d0ec
amir.nurhakim <207092@student.upm.edu.my>
2024-07-03 16:36:44 +0800

    add foreignKey user_id in Brand

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -3,12 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\User;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
  */
-class BrandFactory extends Factory
+class TagFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -18,8 +17,7 @@ class BrandFactory extends Factory
     public function definition(): array
     {
         return [
-            "name"=> fake()->company(),
-            "user_id" => User::factory(),
+            //
         ];
     }
 }
