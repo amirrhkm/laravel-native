e2ed1d3704bc9b106f99ee6024a1989c012ac4b7
amir.nurhakim <207092@student.upm.edu.my>
2024-07-03 10:19:49 +0800

    update variables to use Eloquent ORM

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,10 +3,10 @@
     
     <div class="space-y-4">
         @foreach ($products as $product)
-            <a href="/product/{{ $product['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <a href="/product/{{ $product->id }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
                 <div class="font-bold text-blue-500">{{ $product->brand->name }}</div>
 
-                <strong> {{ $product['name'] }}: </strong> RM {{ $product['price'] }} per unit
+                <strong> {{ $product->name }}: </strong> RM {{ $product->price }} per unit
             </a>
         @endforeach
     </div>
